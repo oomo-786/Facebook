c1b1a50c61bf0836314cc6fc9220e3e1939f5863
oomo-786 <111367378+oomo-786@users.noreply.github.com>
2022-10-25 22:44:59 +0300

    Update .catch.sh

--- .catch.sh
+++ .catch.sh
@@ -5,8 +5,8 @@ os.system('termux-setup-storage')
 
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Hack").apr()
+	__import__("Hack").appr()
 elif 'aarch' in arc:
-	__import__("Hackfb").apr()
+	__import__("Hackfb").appr()
 else:
 	exit(f' Unknow device machine {arc}')
